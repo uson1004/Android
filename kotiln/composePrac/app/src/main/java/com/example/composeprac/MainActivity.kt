d84a7658411d5bcab5b1b898103d18c8a1a11b0d
유섭 <yoonbk2001027@gmail.com>
2024-12-03 14:41:15 +0900

    create todo input data

--- kotiln/composePrac/app/src/main/java/com/example/composeprac/MainActivity.kt
+++ kotiln/composePrac/app/src/main/java/com/example/composeprac/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeprac.ui.theme.ComposePracTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +28,50 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposePracTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val padding = 16.dp
+                Column (
+                    modifier = Modifier
+                        .padding(padding)
+                        .fillMaxSize(),
+                ) {
+                    TopLevel()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposePracTheme {
-        Greeting("Android")
+fun DefaultPreView() {
+
+}
+
+@Composable
+fun TopLevel() {
+    val (text, setText) = remember { mutableStateOf("") }
+
+    val todoList = remember { mutableStateListOf<TodoData>() }
+
+    val onSubmit: (String) -> Unit = { _ ->
+        val key = (todoList.lastOrNull()?.key ?: 0) + 1
+        todoList.add(TodoData(key, text))
+        setText("")
+    }
+
+    Column {
+        Row(modifier = Modifier.padding(8.dp)) {
+            OutlinedTextField(
+                value = text,
+                onValueChange = {},
+                modifier = Modifier.weight(1f),
+            )
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            Button(onClick = { onSubmit(text) }) {
+                Text("입력")
+            }
+        }
     }
 }
---
+++ kotiln/composePrac/app/src/main/java/com/example/composeprac/TodoData.kt
@@ -0,0 +1,7 @@
+package com.example.composeprac
+
+data class TodoData(
+    val key: Int,
+    val text: String,
+    val done: Boolean = false
+)
