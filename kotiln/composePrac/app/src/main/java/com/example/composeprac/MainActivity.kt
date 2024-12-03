a4abff34152eaa505ddc9b9cbea168295c3ad933
유섭 <yoonbk2001027@gmail.com>
2024-12-03 15:13:08 +0900

    create gradient inputfield

--- kotiln/composePrac/app/src/main/java/com/example/composeprac/MainActivity.kt
+++ kotiln/composePrac/app/src/main/java/com/example/composeprac/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.composeprac
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.VectorConverter
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,16 +13,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeprac.ui.theme.ComposePracTheme
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +38,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposePracTheme {
-                val padding = 16.dp
-                Column (
-                    modifier = Modifier
-                        .padding(padding)
-                        .fillMaxSize(),
+                Surface (
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     TopLevel()
                 }
@@ -41,29 +48,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreView() {
-
+fun BrushGradient() { // wow rainbows
+    val rainbowColors: List<Color> = listOf(
+        Color.Red,
+        Color.Yellow,
+        Color.Green,
+        Color.Blue,
+    )
+    var text by remember { mutableStateOf("") }
+    val brush = remember {
+        Brush.linearGradient(
+            colors = rainbowColors
+        )
+    }
+    TextField(
+        value = text, onValueChange = {text = it}, textStyle = androidx.compose.ui.text.TextStyle(
+            brush = brush
+        ),
+    )
 }
 
 @Composable
-fun TopLevel() {
-    val (text, setText) = remember { mutableStateOf("") }
+fun TopLevel() { // crud
+    var text by remember { mutableStateOf("") }
 
     val todoList = remember { mutableStateListOf<TodoData>() }
 
-    val onSubmit: (String) -> Unit = { _ ->
+    val onSubmit: (String) -> Unit = { text ->
         val key = (todoList.lastOrNull()?.key ?: 0) + 1
         todoList.add(TodoData(key, text))
-        setText("")
     }
 
     Column {
         Row(modifier = Modifier.padding(8.dp)) {
             OutlinedTextField(
                 value = text,
-                onValueChange = {},
+                onValueChange = { text = it},
                 modifier = Modifier.weight(1f),
             )
 
