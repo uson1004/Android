ad5d16433b99c30253fd3f875271644a61d65197
유섭 <yoonbk2001027@gmail.com>
2024-12-18 22:34:38 +0900

    create state composition component

--- kotiln/ViewModelPrac2/app/src/main/java/com/example/viewmodelprac/MainActivity.kt
+++ kotiln/ViewModelPrac2/app/src/main/java/com/example/viewmodelprac/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -12,10 +13,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -41,18 +43,57 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableIntStateOf(0) }
+        var count by rememberSaveable { mutableIntStateOf(0) }
 
-        if (count < 0) {
+        if (count > 0) {
+            var showTask by rememberSaveable { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 minute walk today?",
+                    modifier = modifier.padding(top = 8.dp)
+                )
+            }
             Text(text = "You've had $count glasses.")
         }
-        Button(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp)) {
-            Text("Add one")
+
+        Row(modifier = Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                Text("Clear water count")
+            }
         }
     }
 }
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0)
+            Text("You've had $count glasses.")
+        Button(onClick = onIncrement, modifier = Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    var count2 by rememberSaveable { mutableStateOf(0) }
+
+
+    StatelessCounter(count, { count++ }, modifier)
+    StatelessCounter(count2, { count2++ }, modifier)
+    
 }
